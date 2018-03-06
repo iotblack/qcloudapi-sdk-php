b2a2d5c6a37080607947e93830b99489e8b7fc94
huxinbang <huxinbang@gmail.com>
2018-03-06 15:43:38 +0800

    v1

--- controller.php
+++ controller.php
@@ -3,18 +3,23 @@
 <head>
   <meta charset="UTF-8">
   <title>Bootstrap contact form with validation and AJAX submit</title>
-  <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>
+      <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css'>
   <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Lato:300,400,700'>
+  <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap-switch/3.3.4/css/bootstrap3/bootstrap-switch.css'>
+
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/js/bootstrap.min.js'></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/bootstrap-switch/3.3.4/js/bootstrap-switch.js'></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/1000hz-bootstrap-validator/0.11.9/validator.min.js'></script>
+    <script src="js/index.js"></script>
   <link rel="stylesheet" href="css/style.css">
 </head>
   <body>
   <div class="container">
     <div class="row">
-      <div class="col-lg-8 col-lg-offset-2">
-        <h1>Controller(App)</h1> 
-        <p class="lead">Iot 设备控制端。</p> 
+      <div class="col-lg-8 col-lg-offset-2" id='panel_settings'>
+        <H2>参数配置</H2> 
         <form id="controller-form" method="post" action="controller.php" role="form"> 
-        <div class="messages"></div> 
         <div class="controls">
 
           <div class="row">
@@ -61,20 +66,36 @@
               <div class="form-group">
                   <input type="button" class="btn btn-success btn-send" value="读取影子" id="btn_get_iot_shadow">
               </div>
+              <div class="get_messages"></div> 
              </div>
-            <div class="col-md-6">
-              <div class="form-group">
-                  <input type="button" class="btn btn-success btn-send" value="更新影子" id="btn_update_iot_shadow">
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md-12">
-              <p class="text-muted">演示页面</p>
-            </div>
-          </div>
         </div>
+        </form>
+      </div>
 
+      </div>
+      <div class="col-lg-8 col-lg-offset-2" id='panel_shadow'>
+        <form id="shadow-form" method="post" action="" role="form"> 
+            <div class="controls">
+            <div class="row">
+
+            <div class="col-md-6">
+                    <H2>控制面板</H2>
+                    <div class="form-group" id='shadow_container' >
+                    </div>
+            </div>
+            <div class="col-md-6">
+                    <div class="form-group">
+                    <input type="button" class="btn btn-success btn-send" value="更新影子" id="btn_update_iot_shadow">
+                    <div class="update_messages"></div> 
+                    </div>
+
+                    <div class="row">
+                    <div class="col-md-12">
+                    <p class="text-muted">演示页面</p>
+                    </div>
+                    </div>
+            </div>
+            </div>
         </form>
 
       </div>
@@ -83,9 +104,4 @@
 
   </div>
 </body>
-<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js'></script>
-<script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>
-<script src='https://cdnjs.cloudflare.com/ajax/libs/1000hz-bootstrap-validator/0.11.9/validator.min.js'></script>
-<script src="js/index.js"></script>
 </html>
-
--- js/index.js
+++ js/index.js
@@ -6,24 +6,6 @@ $(function() {
     $("#btn_get_iot_shadow").on("click", function(e) {
         // if the validator does not prevent form submit
         if (!e.isDefaultPrevented()) {
-            var url = "controller.php";
-            var messageAlert = "alert-success";
-            var messageText = "请求成功。";
-
-            // let's compose Bootstrap alert box HTML
-            var alertBox =
-                '<div class="alert ' +
-                messageAlert +
-                ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
-                messageText +
-                "</div>";
-
-            // If we have messageAlert and messageText
-            if (messageAlert && messageText) {
-                // inject the alert to .messages div in our form
-                $("#controller-form").find(".messages").html(alertBox);
-            }
-
             var secretid = $('#form_secret_id').val();
             var secretkey  = $('#form_secret_key').val();
             var productId  = $('#form_product_id').val();
@@ -33,8 +15,8 @@ $(function() {
                 url:'/get_iot_shadow.php',
                 data:{
                     secretid :  secretid,
-                    secretkey : secretkey, 
-                    productId : productId, 
+                    secretkey : secretkey,
+                    productId : productId,
                     deviceName : deviceName
                 },
                 type:'post',
@@ -42,15 +24,56 @@ $(function() {
                 datatype:'json',
                 success:function(data) {
                     if(data.code == 0 ){
-                        // alert("读取成功！");
+                        show_get_alert("alert-success", "请求成功。");
                         $('#form_shadow').val(JSON.stringify(data.data.state));
+                        $('#shadow_container').empty();
+                        var reported = data.data.state.reported;
+                        for (var property in reported) {
+                            if (reported.hasOwnProperty(property)) {
+                                var data_type = typeof reported[property];
+                                var ctl_text = '';
+
+                                if (data_type == 'boolean') {
+                                    // ctl_text = '<label for="form_sp_' + property + '" class="form-check-label"><input id="form_sp_'
+                                    //     + property + '" type="checkbox" name="'
+                                    //     + property + '" class="form-check-input" data-type="'
+                                    //     + data_type +'" value="'
+                                    //     + reported[property] + '"> ' + property + '</label><p>';
+                                    var checkstatus = '';
+                                    if (reported[property]) {
+                                        checkstatus = 'checked';
+                                    }
+
+                                    ctl_text = 'property<br><label class="switch"><input id="form_sp_property" name="property" data-type="boolean" checkstatus '
+                                    + 'type="checkbox"> <span class="slider round"></span> </label><br>';
+                                    ctl_text = ctl_text.replace(/property/g, property).replace(/checkstatus/g, checkstatus);
+                                } else if (data_type == 'number') {
+                                    ctl_text = '<label for="form_sp_' + property + '">'
+                                        + property + '</label> <input id="form_sp_'
+                                        + property + '" type="number" name="'
+                                        + property + '" class="form-control" data-type="'
+                                        + data_type +'" value="'
+                                        + reported[property] + '"> ';
+                                } else {
+                                    ctl_text = '<label for="form_sp_' + property + '">'
+                                        + property + '</label> <input id="form_sp_'
+                                        + property + '" type="text" name="'
+                                        + property + '" class="form-control" data-type="'
+                                        + data_type +'" value="'
+                                        + reported[property] + '"> ';
+                                }
+                                $('#shadow_container').append(ctl_text);
+                            }
+                        }
+
+                        $('#panel_settings').hide();
+
                     }else{
-                        alert(data.message);
+                        show_get_alert("alert-danger", data.message);
                     }
                 },
                 error : function() {
-                    // view("异常！");
-                    alert("异常！");
+                    show_get_alert("alert-danger", "未知错误。");
                 }
             });
 
@@ -62,37 +85,41 @@ $(function() {
     $("#btn_update_iot_shadow").on("click", function(e) {
         // if the validator does not prevent form submit
         if (!e.isDefaultPrevented()) {
-            var url = "controller.php";
-
-            var messageAlert = "alert-success";
-            var messageText = "请求成功。";
-
-            // let's compose Bootstrap alert box HTML
-            var alertBox =
-                '<div class="alert ' +
-                messageAlert +
-                ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
-                messageText +
-                "</div>";
-
-            // If we have messageAlert and messageText
-            if (messageAlert && messageText) {
-                // inject the alert to .messages div in our form
-                $("#controller-form").find(".messages").html(alertBox);
-            }
-
             var secretid = $('#form_secret_id').val();
             var secretkey  = $('#form_secret_key').val();
             var productId  = $('#form_product_id').val();
             var deviceName = $('#form_device_name').val();
-            var shadow = $('#form_shadow').val();
+            var shadowObj = {}; 
+            shadowObj['desired'] = {};
+            // $.each($('#shadow-form').serializeArray(), function(_, kv) {
+            //     shadowObj['desired'][kv.name] = kv.value;
+            // });
+
+            $('#shadow-form input, #shadow-form select').each(
+                function(index){
+                    var input = $(this);
+                    if (input.attr('type') == 'button') {
+                        return;
+                    }
+                    //alert('Type: ' + input.attr('type') + 'Name: ' + input.attr('name') + 'Value: ' + input.val());
+                    if (input.attr('data-type') == 'boolean') {
+                        shadowObj['desired'][input.attr('name')] = input.is(':checked'); 
+                    } else if (input.attr('data-type') == 'number') {
+                        shadowObj['desired'][input.attr('name')] = Number(input.val()); 
+                    } else {
+                        shadowObj['desired'][input.attr('name')] = input.val(); 
+                    }
+                }
+            );
+
+            var shadow = JSON.stringify(shadowObj);
 
             var aj = $.ajax( {
                 url:'/update_iot_shadow.php',
                 data:{
                     secretid :  secretid,
-                    secretkey : secretkey, 
-                    productId : productId, 
+                    secretkey : secretkey,
+                    productId : productId,
                     deviceName : deviceName,
                     shadow : shadow,
                 },
@@ -101,15 +128,13 @@ $(function() {
                 datatype:'json',
                 success:function(data) {
                     if(data.code == 0 ){
-                        alert("更新成功！");
-                        // $('#form_shadow').val(JSON.stringify(data.data.state));
+                        show_update_alert("alert-success", "更新成功。");
                     }else{
-                        alert(data.message);
+                        show_update_alert("alert-danger", data.message);
                     }
                 },
                 error : function() {
-                    // view("异常！");
-                    alert("异常！");
+                    show_update_alert("alert-danger", "未知错误。");
                 }
             });
 
@@ -118,3 +143,41 @@ $(function() {
     });
 
 });
+
+// alert-success alert-info alert-danger alert_warning
+function show_get_alert(message_alert, message_text)
+{
+
+    // let's compose Bootstrap alert box HTML
+    var alert_box =
+        '<div class="alert ' +
+        message_alert +
+        ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
+        message_text +
+        "</div>";
+
+    // If we have messageAlert and messageText
+    if (message_alert && message_text) {
+        // inject the alert to .messages div in our form
+        $("#controller-form").find(".get_messages").html(alert_box);
+    }
+}
+
+function show_update_alert(message_alert, message_text)
+{
+
+    // let's compose Bootstrap alert box HTML
+    var alert_box =
+        '<div class="alert ' +
+        message_alert +
+        ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
+        message_text +
+        "</div>";
+
+    // If we have messageAlert and messageText
+    if (message_alert && message_text) {
+        // inject the alert to .messages div in our form
+        $("#controller-form").find(".update_messages").html(alert_box);
+    }
+}
+
